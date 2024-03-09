d57edd963095240cb4a63b39ba1982b08a12eb3e
oihoihoih <ochyuo@hotmail.com>
2024-03-09 14:21:27 +0100

    3rd exercise. Currency exchange implemented

--- PEC1_Ej2_3/script.js
+++ PEC1_Ej2_3/script.js
@@ -3,6 +3,16 @@ const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
+const currencySelect = document.getElementById("currency");
+const currencyOne = document.getElementById("currency").value;
+// const movieCurrency = document.querySelectorAll("#movie option");
+
+const films = [
+  { title: "Avengers Endgame", price: "10", currency: "USD" },
+  { title: "Joker", price: "12", currency: "USD" },
+  { title: "Toy Story 4", price: "8", currency: "USD" },
+  { title: "Lion King", price: "9", currency: "USD" },
+];
 
 populateUI();
 
@@ -46,10 +56,50 @@ function populateUI() {
   }
 }
 
+// Calculate Currency
+function calculateCurrency(event) {
+  //const currencyTwo = document.getElementById("currency").value;
+
+  if (event) {
+    // CurrencyOne === USD
+    // CurrencyTwo === Moneda elegida (ej. EUR)
+    // Event.target.value === USD (o moneda que sea)
+    // Ticket price = precio en USD
+    const currencyTwo = event.target.value;
+    fetch(
+      `https://v6.exchangerate-api.com/v6/5759c061bafab526b0435aa0/latest/${currencyOne}`
+    )
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`There was an error ${res.status}`);
+        }
+        return res.json();
+      })
+
+      .then((data) => {
+        const rate = data.conversion_rates[currencyTwo];
+        // Actualizar todos los precios
+        // ticketPrice = (rate * ticketPrice).toFixed(2);
+        // console.log(ticketPrice);
+        // console.log(movieSelect);
+
+        // To change Option films price and currency
+        for (let i = 0; i < films.length; i++) {
+          console.log(i);
+          movieSelect.options[i].textContent = `${films[i].title} (${(
+            films[i].price * rate
+          ).toFixed(2)} ${currencyTwo})`;
+        }
+      });
+    console.log(event.target.value);
+  }
+}
+
 // Event listeners
 // Movie select event
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
+  console.log(ticketPrice);
   updateSelectedCount();
   setMovieData(e.target.selectedIndex, e.target.value);
 });
@@ -66,5 +116,9 @@ container.addEventListener("click", (e) => {
   }
 });
 
+// Currency select event
+currencySelect.addEventListener("change", calculateCurrency);
+
 // Initial count and total set
 updateSelectedCount();
+calculateCurrency();
